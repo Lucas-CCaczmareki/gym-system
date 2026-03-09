618cd3b5cf6b899e08af9bc256e6c6f51fd3d871
Lucas-CCaczmareki <lucascaczmareki@gmail.com>
2026-03-09 14:45:56 -0300

    week volume calculator

--- src/main/java/gym_backend/dto/WorkoutDTO.java
+++ src/main/java/gym_backend/dto/WorkoutDTO.java
@@ -7,7 +7,7 @@ import lombok.Data;
 @Data // isso usa a biblioteca "lombok" pra criar automaticamente todos getters e setters dos atributos
 public class WorkoutDTO {
     private String name;
-    private String day;
+    // private String day;
     private LocalDate date;
     private List<ExerciseDTO> exercises;
 }
---
+++ src/main/java/gym_backend/service/VolumeCalculator.java
@@ -0,0 +1,62 @@
+package gym_backend.service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+import gym_backend.dto.ExerciseDTO;
+import gym_backend.dto.TargetMuscle;
+import gym_backend.dto.WorkoutDTO;
+import gym_backend.enums.Muscle;
+import gym_backend.enums.MuscleRole;
+
+
+// Maybe I'll have to reset it once a week? dont know how yet
+@Service
+public class VolumeCalculator { 
+    //Atributos
+    private List<WorkoutDTO> week = new ArrayList<>(); // receives a week of training   
+    private Map<Muscle, Double> volume = new HashMap<>();
+    
+    public void addWorkout(WorkoutDTO workout) {
+        week.add(workout);  // add a workout to the week
+    }
+
+    // There is a problem here, if the user calls it twice it will calculate again, even if its the same workout
+    public Map<Muscle, Double> weekVolume() {
+        // Reset map
+        volume = new HashMap<>();
+
+        // Percorrer todos os treinos da semana
+        for(WorkoutDTO workout : week) {
+            // Percorrer o exercises do dto
+            for(ExerciseDTO exercise : workout.getExercises()) {
+                // Percorrer o vetor de target e somar 1.0*sets.size se for primary ou 0.5*sets.size se for secondary
+                for(TargetMuscle target : exercise.getTarget()) {
+                    Muscle muscle = target.getMuscle();
+                    Double muscle_vol = (double) exercise.getSets().size();
+
+                    // Debugging
+
+
+                    
+                    if(target.getRole() == MuscleRole.PRIMARY) {
+                        // The function merge receives the key, the value and a function
+                        // If the place you are trying to update already has a value, it will merge
+                        // using the function you specified. This bi function receives two params
+                        // and make an operation using them, in this case, it sum the old with the new.
+                        volume.merge(muscle, muscle_vol*1.0, (oldValue, newValue) -> oldValue + (newValue*1.0));
+                    } else { //se não for, esse músculo é secundário
+                        volume.merge(muscle, muscle_vol*0.5, (oldValue, newValue) -> oldValue + (newValue*0.5));
+                    }
+                }        
+            } 
+        }
+
+        return volume;
+    }
+
+}
